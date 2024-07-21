7aa8a71130d0ea5b5a4e615f8fc1f386c8e7c995
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-21 11:33:05 +0530

    Added code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module todo
 
 go 1.22.5
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -4,95 +4,83 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Todo struct {
-	ID        int    `json:"id"`
-	Task      string `json:"task"`
-	IsCompleted bool `json:"isCompleted"`
+	ID          int    `json:"id"`
+	Task        string `json:"task"`
+	IsCompleted bool   `json:"isCompleted"`
 }
 
-var (
-	todos  = []Todo{}
-	nextID = 1
-	mu     sync.Mutex
-)
+var todos []Todo
+var idCounter int
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	mu.Lock()
-	defer mu.Unlock()
 	json.NewEncoder(w).Encode(todos)
 }
 
 func createTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var todo Todo
-	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	mu.Lock()
-	defer mu.Unlock()
-	todo.ID = nextID
-	nextID++
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	idCounter++
+	todo.ID = idCounter
 	todos = append(todos, todo)
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(todo)
 }
 
 func updateTodo(w http.ResponseWriter, r *http.Request) {
-	var updatedTodo Todo
-	if err := json.NewDecoder(r.Body).Decode(&updatedTodo); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	mu.Lock()
-	defer mu.Unlock()
-	for i, todo := range todos {
-		if todo.ID == updatedTodo.ID {
-			todos[i] = updatedTodo
-			json.NewEncoder(w).Encode(updatedTodo)
+
+	for index, item := range todos {
+		if item.ID == id {
+			todos = append(todos[:index], todos[index+1:]...)
+			var todo Todo
+			_ = json.NewDecoder(r.Body).Decode(&todo)
+			todo.ID = id
+			todos = append(todos, todo)
+			json.NewEncoder(w).Encode(todo)
 			return
 		}
 	}
-	http.NotFound(w, r)
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	var todo Todo
-	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	mu.Lock()
-	defer mu.Unlock()
-	for i, t := range todos {
-		if t.ID == todo.ID {
-			todos = append(todos[:i], todos[i+1:]...)
+
+	for index, item := range todos {
+		if item.ID == id {
+			todos = append(todos[:index], todos[index+1:]...)
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 	}
-	http.NotFound(w, r)
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func main() {
-	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			getTodos(w, r)
-		case "POST":
-			createTodo(w, r)
-		case "PUT":
-			updateTodo(w, r)
-		case "DELETE":
-			deleteTodo(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-
-	log.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/todos", getTodos).Methods("GET")
+	r.HandleFunc("/todos", createTodo).Methods("POST")
+	r.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	r.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
